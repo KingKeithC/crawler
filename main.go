64750f57fc3531820794a8a5f6bd7c3789f63005
Keith King <keith.king@cgi.com>
2019-11-23 22:25:08 -0700

    Add functionality to write visited and unvisited URLs to files upon completion.

--- main.go
+++ main.go
@@ -1,13 +1,32 @@
 package main
 
-import "fmt"
+import (
+	"bytes"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+)
 
 func main() {
-	originalUrL := "https://en.wikipedia.org/wiki/Main_Page"
+	originalUrL := "https://twitter.com/"
 	var iterations uint32 = 250
 
 	crawler := NewCrawler(1, nil, nil)
 	visited, unvisited := crawler.CrawlNRecursively(originalUrL, iterations)
+
+	visitedBytes := strSliceToByteSlice(visited)
+	unvisitedBytes := strSliceToByteSlice(unvisited)
+
+	err := ioutil.WriteFile("visited.txt", visitedBytes, os.ModePerm)
+	if err != nil {
+		log.Fatalf("Error writing visited.txt")
+	}
+
+	err = ioutil.WriteFile("unvisited.txt", unvisitedBytes, os.ModePerm)
+	if err != nil {
+		log.Fatalf("Error writing unvisited.txt")
+	}
 
 	fmt.Printf("***********************************\n\n")
 	fmt.Printf("The Original Url was:\n")
@@ -16,4 +35,27 @@ func main() {
 	fmt.Printf("%8d: URLs were Visited, and\n", len(visited))
 	fmt.Printf("%8d: URLs are Unvisited.\n\n", len(unvisited))
 	fmt.Printf("***********************************\n")
+
+}
+
+func strSliceToByteSlice(strSlice []string) []byte {
+	// Create a buffer to store our bytes in
+	buf := &bytes.Buffer{}
+
+	// For each string, add it to the buffer
+	for i, str := range strSlice {
+		// If its not the first string, append a newline to the beginning
+		if i != 0 {
+			str = fmt.Sprintf("\n%s", str)
+		}
+
+		// Write the string to the buffer, and check for errors
+		_, err := buf.WriteString(str)
+		if err != nil {
+			log.Fatalf("Error writing string %s to buffer %v", str, *buf)
+		}
+	}
+
+	// Return the contents of the buffer as bytes
+	return buf.Bytes()
 }
