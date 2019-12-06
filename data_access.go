9faab5250713711ed0fc72726223c920704a80c0
Keith King <KingKeithC@gmail.com>
2019-12-05 20:49:02 -0700

    improve logging

--- data_access.go
+++ data_access.go
@@ -10,6 +10,9 @@ import (
 
 // InitDB creates and tests a connection to the SQL DB
 func InitDB(conStr string) (*sql.DB, error) {
+	log.Infof("Initializing Database...")
+
+	// Create the DB Object
 	db, err := sql.Open("postgres", conStr)
 	if err != nil {
 		return nil, fmt.Errorf("error %v creating sql.DB object with"+
@@ -22,18 +25,19 @@ func InitDB(conStr string) (*sql.DB, error) {
 		return nil, fmt.Errorf("error %v pinging DB %v", err, db)
 	}
 
+	// Create the Schema
+	err = CreateSchema(db)
+	if err != nil {
+		return nil, fmt.Errorf("error %v creating schema", err)
+	}
+
+	log.Infof("Database Initialized.")
 	return db, nil
 }
 
 // CreateSchema connects to the DB and ensures that the schema exists
 func CreateSchema(db *sql.DB) error {
-	tableSchema := `
-	CREATE TABLE urls (
-		id serial PRIMARY KEY,
-		url TEXT,
-		visited BOOLEAN
-	);`
-
+	// Start a transaction to keep the modifications
 	tx, err := db.Begin()
 	if err != nil {
 		if rollbackErr := tx.Rollback(); rollbackErr != nil {
@@ -42,6 +46,13 @@ func CreateSchema(db *sql.DB) error {
 		return fmt.Errorf("error %v while beginning DB transaction", err)
 	}
 
+	// Create the table schema
+	tableSchema := `
+	CREATE TABLE IF NOT EXISTS urls (
+		id serial PRIMARY KEY,
+		url TEXT,
+		visited BOOLEAN
+	);`
 	_, err = tx.Exec(tableSchema)
 	if err != nil {
 		if rollbackErr := tx.Rollback(); rollbackErr != nil {
@@ -50,11 +61,12 @@ func CreateSchema(db *sql.DB) error {
 		return fmt.Errorf("error %v crating DB schema %s", err, tableSchema)
 	}
 
+	// Commit the transaction
 	err = tx.Commit()
 	if err != nil {
 		return fmt.Errorf("error %v while commiting DB transaction", err)
 	}
 
-	log.Infof("Initializing DB")
+	log.Infof("Schema Initialized.")
 	return nil
 }
